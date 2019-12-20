a601fc472590f06c2720eb99e28f3cc111e9a698
sunilnaik <vsunildhawanganesh@gmail.com>
2019-12-20 15:08:45 +0530

    hello

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT|| 3000
+const port = process.env.PORT || 3000
 app.use(express.static('frontend'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
 app.get('/login', (req, res) => res.send('login'))
